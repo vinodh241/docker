24b9ede69a2260470029ae978458f759029506a8
vinodh241 <a.vinodh9072@gmail.com>
2025-07-15 21:11:45 +0530

    docker

---
+++ docker.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
+
+xfs_growfs /
+xfs_growfs /var
